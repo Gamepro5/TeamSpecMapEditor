b322bb1577ef24c8f2def76e363307f09e0f658f
Gamepro5 <gamepro543@gmail.com>
2024-03-05 20:53:42 -0800

    First Commit

---
+++ maps/my_map/logic.gd
@@ -0,0 +1,23 @@
+extends MapLogicTemplate
+
+func get_spawn_point_position_for_team(teamNumber):
+	if teamNumber == 1:
+		$Team1SpawnPoints.get_children().pick_random()
+		var spawnPoint =  $Team1SpawnPoints.get_children().pick_random()
+		return spawnPoint.global_position
+	elif teamNumber == 2:
+		var spawnPoint = $Team2SpawnPoints.get_children().pick_random()
+		return spawnPoint.global_position
+	else:
+		return Vector3.ZERO
+
+func assign_player(id):
+	if multiplayer.is_server():
+		if team1.size() > team2.size():
+			team2[id] = 1
+			characterInstances[id].team = 2
+			characterInstances[id].set_pos(get_spawn_point_position_for_team(2))
+		else:
+			team1[id] = 1
+			characterInstances[id].team = 1
+			characterInstances[id].set_pos(get_spawn_point_position_for_team(1))
