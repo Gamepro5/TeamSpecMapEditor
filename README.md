3e2fc2950a146ac3011f271e8068e83955bed9a9
Gamepro5 <gamepro543@gmail.com>
2024-03-07 16:19:45 -0800

    Upload Tutorial

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# TeamSpecMap
+# TeamSpec Map Editor
 This is a pre-packaged bundle of dependencies to allow for map making for TeamSpec
 
-To use the Cyclops Level Editor addon, see [this tutorial](https://youtu.be/mbw_6dnOt_g)
+SEE [THIS TUTORIAL](https://youtu.be/KOH34_kJfkM) FIRST!
 
-
-To test out your map in TeamSpec, see [this tutorial](https://youtu.be/YtPzoLtE7R0).
+To use the Cyclops Level Editor addon, see [this tutorial](https://youtu.be/mbw_6dnOt_g).
 
 
--- maps/my_map/logic.gd
+++ maps/my_map/logic.gd
@@ -2,11 +2,11 @@ extends MapLogicTemplate
 
 func get_spawn_point_position_for_team(teamNumber):
 	if teamNumber == 1:
-		$Team1SpawnPoints.get_children().pick_random()
-		var spawnPoint =  $Team1SpawnPoints.get_children().pick_random()
+		$Props/Team1SpawnPoints.get_children().pick_random()
+		var spawnPoint =  $Props/Team1SpawnPoints.get_children().pick_random()
 		return spawnPoint.global_position
 	elif teamNumber == 2:
-		var spawnPoint = $Team2SpawnPoints.get_children().pick_random()
+		var spawnPoint = $Props/Team2SpawnPoints.get_children().pick_random()
 		return spawnPoint.global_position
 	else:
 		return Vector3.ZERO
@@ -14,10 +14,11 @@ func get_spawn_point_position_for_team(teamNumber):
 func assign_player(id):
 	if multiplayer.is_server():
 		if team1.size() > team2.size():
-			team2[id] = 1
+			team2[id] = 1 #it's value doesn't matter, we just care that it 
 			characterInstances[id].team = 2
 			characterInstances[id].set_pos(get_spawn_point_position_for_team(2))
 		else:
-			team1[id] = 1
+			team1[id] = 1 #it's value doesn't matter, we just care that it 
 			characterInstances[id].team = 1
 			characterInstances[id].set_pos(get_spawn_point_position_for_team(1))
+
